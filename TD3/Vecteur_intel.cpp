ee6ed4d0d9d8bbb9e79b55b7070c9a0555b9d473
Javier Morales <javi_morales@hotmail.es>
2021-03-27 21:11:41 +0100

    Vecteur_intel class has been added

--- TD3/Vecteur_intel.cpp
+++ TD3/Vecteur_intel.cpp
@@ -1,11 +1,43 @@
 #include "Vecteur_intel.h"
 
-Vecteur_intel & Vecteur_intel::operator = (const Vecteur_intel & vec) {
-    if (dim > vec.dim){
-        
-    } 
-    for (int i = 0; i<dim; i++){
-        elements[i] = vec.elements[i];
-    }
-    return (*this);	
+
+// affectation 
+Vecteur_intel & Vecteur_intel::operator= (const Vecteur_intel & operande1){  
+    int i;  
+    if(dim != operande1.dim){    
+        if (dim!=0) delete [] elements;    
+        if(!(elements=new float[operande1.dim])){      
+            cout << "erreur allocation \n";      
+            exit(0);    
+        }    
+    dim=operande1.dim;  
+    }  
+    for (i=0;i<dim;i++){    
+        elements[i]=operande1.elements[i];  
+    }  
+    return(*this);
 }
+
+// +=
+Vecteur_intel & Vecteur_intel::operator+= (Vecteur_intel & operande){  
+    int i;
+    if (dim < operande.dim){    
+        float * tab;    
+        if(!(tab=new float[operande.dim])){      
+            cout << "erreur allocation \n";      
+            exit(0);    
+        }    
+        for (i=0;i<dim;i++) tab[i]=operande.elements[i]+elements[i];    
+        for (i=dim;i<operande.dim;i++) tab[i]=operande.elements[i];    
+        delete [] elements;    
+        elements=tab;    
+        dim=operande.dim;   
+    } else { 
+        for (i=0;i<operande.dim;i++) elements[i]+=operande.elements[i];  
+    }
+    return (*this);
+}
+
+Vecteur_intel  operator + (Vecteur_intel & operande1, Vecteur_intel & operande2){  
+    Vecteur_intel temp_vect(operande1);  
+    return(temp_vect += operande2);}
--- TD3/Vecteur_intel.h
+++ TD3/Vecteur_intel.h
@@ -1,12 +1,20 @@
 #include "Vecteur.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Vecteur_intel : public Vecteur {
 	public:
         // Constructors
-		Vecteur_intel(int dim1):Vecteur(dim1) {}
-		Vecteur_intel(int dim1, float val_init):Vecteur(dim1, val_init) {}
-		Vecteur_intel(const Vecteur_intel &v):Vecteur(v) {}
+		Vecteur_intel(int dim_vect): Vecteur(dim_vect){};  
+		Vecteur_intel(): Vecteur(0,0){};  
+		Vecteur_intel(int dim_vect,float val_init ):Vecteur ( dim_vect, val_init){};  
+		Vecteur_intel(const Vecteur_intel & autre):Vecteur (autre){};
 	
         // Operators
-		Vecteur_intel & operator = (const Vecteur_intel & vec);
+		Vecteur_intel & operator= ( const Vecteur_intel & operande1);  
+		Vecteur_intel & operator+= (Vecteur_intel & operande);  
+		
+		// fonction  
+		virtual inline string getname(){return string("classe vecteur_intel");}
 };
